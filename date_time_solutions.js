b29d3b4c645613930f83ad748608ba2d491d852e
Mathias Fischer <mfischerf92@gmail.com>
2023-03-20 18:24:39 -0500

    code wars solution for 3/20/2023, felt pretty good about this one

--- date_time_solutions.js
+++ date_time_solutions.js
@@ -1,12 +1,21 @@
 //3/17/2023
-var min = function(list){
-    list.sort((a,b) => a > b ? 1 : -1)
-    return list[0];
-}
+// var min = function(list){
+//     list.sort((a,b) => a > b ? 1 : -1)
+//     return list[0];
+// }
 
-var max = function(list){
-    list.sort((a,b) => a < b ? 1 : -1)
-    return list[0];
-}
+// var max = function(list){
+//     list.sort((a,b) => a < b ? 1 : -1)
+//     return list[0];
+// }
 
-console.log(min([2,5,7,3,4,5,8,23,12,33456,6346,342,2,342,236617,456]))
+// console.log(min([2,5,7,3,4,5,8,23,12,33456,6346,342,2,342,236617,456]))
+
+
+//3/20/2023
+
+// function descendingOrder(n){
+//     if(n>0){
+//     return Number(n.toString().split('').sort((a,b) => b-a).join(''));
+//     } else return n
+// }
