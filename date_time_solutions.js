5ebd2416340738548b3357434a609a81c2a91751
Mathias Fischer <mfischerf92@gmail.com>
2023-03-21 17:51:59 -0500

    solution for 3/21/2023 added

--- date_time_solutions.js
+++ date_time_solutions.js
@@ -19,3 +19,17 @@
 //     return Number(n.toString().split('').sort((a,b) => b-a).join(''));
 //     } else return n
 // }
+
+
+//3/21/2023
+
+function getCount(str) {
+    let c = 0
+    let arr = str.split("")
+    for (let i = 0; i<arr.length; i++){
+        if(arr[i]== 'a' || arr[i]== 'e' || arr[i]=='i' || arr[i]=='o' || arr[i]=='u'){
+            c++
+        }
+    }
+    return c
+  }
