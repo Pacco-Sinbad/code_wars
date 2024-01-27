d129e07fee5408e79d97553330d464962ce97825
Mathias Fischer <mfischerf92@gmail.com>
2024-01-27 17:56:51 -0600

    simple conditional statement

--- date_time_solutions.js
+++ date_time_solutions.js
@@ -856,3 +856,20 @@
 //   };
 //   return solution
 // }
+
+// 01/27/2024
+
+function likes(names) {
+  if(names.length === 0){
+    return "no one likes this"
+  }else if(names.length === 1){
+    return `${names[0]} likes this`
+  }else if(names.length === 2){
+    return `${names[0]} and ${names[1]} like this`
+  }else if(names.length === 3){
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  }else {
+    let extras = names.length - 2
+    return `${names[0]}, ${names[1]} and ${extras} others like this`
+  }
+}
